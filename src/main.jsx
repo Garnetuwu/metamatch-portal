4e837bc92f2b5018c37ddcb89929626830e6c320
Garnet Ni <garnet2333@hotmail.com>
2023-03-25 19:22:23 +0000

    minor change for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import router from "./router";
